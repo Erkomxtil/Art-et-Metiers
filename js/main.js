551e553c91512e22bad8271ec19e769d1b02be0b
Stéphane Chaffre <erkomxtil@gmail.com>
2020-04-24 01:16:22 +0200

    ajout logo école map, reglage logo partner responsive, animation carte

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* Affichage des informations pour la carte */
+const btnCampus = () => {
+  let btn = document.querySelector(".btnCampus") /* bouton du campus */
+  let infosPlus = document.querySelector(".infosMap") /*texte affiché */
+  let mapBright = document.querySelector(".map figure") /* Contrôle sur la brillance de la carte */
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if(infosPlus.className === "infosMap active"){
+      infosPlus.classList.remove("active")
+      mapBright.style.filter = "brightness(1)"
+    } else {
+      infosPlus.classList.add("active")
+      mapBright.style.filter = "brightness(0.2)"
+    }
+  })
+}
+
+btnCampus()
