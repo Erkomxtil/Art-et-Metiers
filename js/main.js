608dc2e158f0dabaabb9642bbe7f0037f2ae7437
Stéphane Chaffre <erkomxtil@gmail.com>
2020-04-25 16:36:17 +0200

    mise en place de la flèche pour le scroll et nettoyage de code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 /* Affichage des informations pour la carte ----------------------------- */
 function btnCampus () {
   /* bouton du campus */
-  var btn = document.querySelector(".btnCampus");
+  var btn = document.querySelector(".btnCampus")
   /*texte affiché */
-  var infosPlus = document.querySelector(".infosMap");
+  var infosPlus = document.querySelector(".infosMap")
   /* Contrôle sur la brillance de la carte */
-  var mapBright = document.querySelector(".map figure");
+  var mapBright = document.querySelector(".map figure")
 
   btn.addEventListener("click", function (e){
-    e.preventDefault();
+    e.preventDefault()
 
     if(infosPlus.className === "infosMap active"){
-      infosPlus.classList.remove("active");
-      mapBright.style.filter = "brightness(1)";
+      infosPlus.classList.remove("active")
+      mapBright.style.filter = "brightness(1)"
     } else {
-      infosPlus.classList.add("active");
+      infosPlus.classList.add("active")
       btn.classList.add("out");
-      mapBright.style.filter = "brightness(0.2)";
+      mapBright.style.filter = "brightness(0.2)"
     }
   })
 }
@@ -45,85 +45,91 @@ var imageTableau = [
   }   
 ]
 var indicateurImage = 0;
-var imageBackground = document.getElementById("slider1");
-var wrapper = document.createElement("div");
-var listeMenu = document.createElement("ul");
+var imageBackground = document.getElementById("slider1")
+var wrapper = document.createElement("div")
+var listeMenu = document.createElement("ul")
 var lien1 = document.createElement("li")
 var lien2 = document.createElement("li")
 var lien3 = document.createElement("li")
-var title = document.createElement("h1");
-var paragraphe = document.createElement("p");
+var title = document.createElement("h1")
+var paragraphe = document.createElement("p")
 var lireplus = document.createElement("p")
+var fleche = document.createElement("div")
 
 /* L'affichage de la 1ere image --*/
 /*liens */
-lien1.innerHTML = '<a href="#">Ecole</a>';
-lien2.innerHTML = '<a href="#">Robotique</a>';
-lien3.innerHTML = '<a href="#">Industrie du futur</a>';
-listeMenu.appendChild(lien1);
-listeMenu.appendChild(lien2);
-listeMenu.appendChild(lien3);
-lien1.className = "lienActif";
+lien1.innerHTML = '<a href="#">Ecole</a>'
+lien2.innerHTML = '<a href="#">Robotique</a>'
+lien3.innerHTML = '<a href="#">Industrie du futur</a>'
+listeMenu.appendChild(lien1)
+listeMenu.appendChild(lien2)
+listeMenu.appendChild(lien3)
+lien1.className = "lienActif"
 
 /* Titre */
-title.textContent = imageTableau[0].title;
+title.textContent = imageTableau[0].title
 
 /* paragraphe */
-paragraphe.textContent = imageTableau[0].text;
+paragraphe.textContent = imageTableau[0].text
 
 /* lien lire plus */
-lireplus.innerHTML = '<a href="#">Lire plus</a>';
+lireplus.innerHTML = '<a href="#">Lire plus</a>'
 lireplus.className = "lirePlus"
+
+/* fleche pour scroller vers le bas */ 
+fleche.className = "fleche"
 
 /* Infos affichées dans la div slider1 */
 wrapper.className = "infosSlider1"
-wrapper.appendChild(listeMenu);
-wrapper.appendChild(title);
-wrapper.appendChild(paragraphe);
-wrapper.appendChild(lireplus);
-imageBackground.appendChild(wrapper);
+wrapper.appendChild(listeMenu)
+wrapper.appendChild(title)
+wrapper.appendChild(paragraphe)
+wrapper.appendChild(lireplus)
+imageBackground.appendChild(wrapper)
+imageBackground.appendChild(fleche)
 
 
 /* affichage des éléments du slider --*/
 function slider1 () {
   if(indicateurImage < imageTableau.length -1){
-    indicateurImage++;
+    indicateurImage++
   }
   else {
-    indicateurImage = 0;
+    indicateurImage = 0
   }
 
 
-  imageBackground.style.backgroundImage = 'url("'+ imageTableau[indicateurImage].image +'")';
-  title.textContent = imageTableau[indicateurImage].title;
-  paragraphe.textContent = imageTableau[indicateurImage].text;
+  imageBackground.style.backgroundImage = 'url("'+ imageTableau[indicateurImage].image +'")'
+  title.textContent = imageTableau[indicateurImage].title
+  paragraphe.textContent = imageTableau[indicateurImage].text
 
 
-  wrapper.appendChild(title);
-  wrapper.appendChild(paragraphe);
+  wrapper.appendChild(title)
+  wrapper.appendChild(paragraphe)
   wrapper.appendChild(lireplus)
+  imageBackground.appendChild(fleche)
 
   /* Gestion de l'onglet en cours */
   var id = imageTableau[indicateurImage].id 
 
   switch (id) {
     case 1:
-      lien1.className = "lienActif";
-      lien2.className = "";
-      lien3.className = "";
+      lien1.className = "lienActif"
+      lien2.className = ""
+      lien3.className = ""
     break;
     case 2:
-      lien1.className = "";
-      lien2.className = "lienActif";
-      lien3.className = "";
-    break;
+      lien1.className = ""
+      lien2.className = "lienActif"
+      lien3.className = ""
+    break
     case 3:
-      lien1.className = "";
-      lien2.className = "";
-      lien3.className = "lienActif";
-    break;
+      lien1.className = ""
+      lien2.className = ""
+      lien3.className = "lienActif"
+    break
     default:
-      lien1.className = "lienActif";
+      lien1.className = "lienActif"
   }
 }
-setInterval("slider1()", 8000);
+setInterval("slider1()", 8000)
